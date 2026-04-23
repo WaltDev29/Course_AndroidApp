6bef6a4083afe735fc70fdea24362eef5ba90a66
WaltDev29 <waltdev29@gmail.com>
2026-04-23 14:45:11 +0900

    # 260423 - Intent & Activity - putExtra & GetExtra

---
+++ 11_intent/app/src/main/java/kr/ac/kopo/a11_intent/MainActivity.java
@@ -0,0 +1,38 @@
+package kr.ac.kopo.a11_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnMain = findViewById(R.id.btn_main);
+
+        editName = findViewById(R.id.editName);
+
+        btnMain.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            intent.putExtra("userName", editName.getText().toString());
+            startActivity(intent);
+        });
+    }
+}
---
+++ 11_intent/app/src/main/java/kr/ac/kopo/a11_intent/MainActivity2.java
@@ -0,0 +1,39 @@
+package kr.ac.kopo.a11_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textResult = findViewById(R.id.textResult);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+        textResult.setText(userName + "님 환영합니다!");
+
+        Button btnMain2 = findViewById(R.id.btn_main2);
+        btnMain2.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
